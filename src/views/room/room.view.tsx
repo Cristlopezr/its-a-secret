22443eff4e636fda5cc4cbda7dd3db1637df2395
Cristian <cristianlopezr.931@gmail.com>
2025-12-28 17:49:39 -0300

    Updated readme

--- src/views/room/room.view.tsx
+++ src/views/room/room.view.tsx
@@ -1,8 +1,7 @@
 import Loader from '@/components/ui/loader';
 import { socket } from '@/lib/socket';
-import { useGameActions, useGameSessionState, useGameRoom, useGameSinglePlayer } from '@/stores/game.store';
-import { useEffect, useRef, useState } from 'react';
-import { useNavigate } from 'react-router';
+import { useGameActions, useGameSessionState, useGameSinglePlayer } from '@/stores/game.store';
+import { useEffect, useState } from 'react';
 import { EnterNameView } from './enter-name.view';
 import { GameRoomView } from './game-room.view';
 
@@ -10,10 +9,7 @@ export const RoomView = () => {
     const [showRoomView, setShowRoomView] = useState(false);
     const sessionState = useGameSessionState();
     const { setRoom } = useGameActions();
-    const room = useGameRoom();
     const singlePlayer = useGameSinglePlayer();
-    const navigate = useNavigate();
-    const shouldNavigate = useRef(false);
 
     //TODO:Checkear por los parametros si la room existe
     useEffect(() => {
@@ -30,20 +26,9 @@ export const RoomView = () => {
         };
     }, []);
 
-   /*  useEffect(() => {
-        if (shouldNavigate.current) {
-            navigate('/');
-        }
-    }, [shouldNavigate.current]);
- */
     if (sessionState === 'checking') {
         return <Loader />;
     }
-
-    /* if (!singlePlayer || (room.status !== 'waitingPlayers' && room.status !== 'waitingSecrets')) {
-        shouldNavigate.current = true;
-        return null;
-    } */
 
     if (!showRoomView && !singlePlayer?.username) {
         return <EnterNameView />;
